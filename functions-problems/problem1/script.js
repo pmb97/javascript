d243088489845fe79713ce911ad3ae7b57306e63
PrisM <82834790+pmb97@users.noreply.github.com>
2021-06-07 16:06:58 -0500

    javascript

--- functions-problems/problem1/script.js
+++ functions-problems/problem1/script.js
@@ -3,6 +3,7 @@
 
 
 /*
+
 step 1: define a Javascript functions that accepts a string as a parameter
 step 2: using the split method, split the string passed in and createan array of each words
 step 3: Define a variable called longestWord and set its intial value to an empty string (without space)
@@ -10,6 +11,7 @@ step 4: Set up a for loop that compares the array of words
 step 5: Check the length of each word and compare it to the word stored in the variable longestWord
 step 6: Store the new longest word in the variable longestWord until the loop is completed
 step 7: Return the longestWord
+
 */
 
 function findLongestWord(str){
