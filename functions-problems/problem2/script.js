81366e39c50f11f2436a9ad19acff6157d71e314
PrisM <82834790+pmb97@users.noreply.github.com>
2021-06-09 21:13:01 -0500

    Fix for loop statement to define type of variable, complete conditional statement, add block of logic to execute when statement is true and correctly set up/return variable that will keep track of how many vowels are in the string.

--- functions-problems/problem2/script.js
+++ functions-problems/problem2/script.js
@@ -7,7 +7,7 @@
 step 1: define a Javascript functions that accepts a string as a parameter
 step 2: using the split method, split the string passed in and create an array of individual letters,
 store it in a variable called arrayOfLetters
-step 3: Define a variable called numberOfVowels and set its intial value to be equal to 0
+step 3: Define a variable called vowelCount and set its intial value to be equal to 0
 step 4: Define a variable that defines what a vowel is, an array of vowels
 step 5: Start looping through our array of letters
 step 6: Check if the current letter in the loop is a vowel
@@ -22,13 +22,14 @@ function findVowelsCount(str){
 
   var arrayOfLetters = str.split("");
   var arrayOfVowels = ["a", "e", "i", "o", "u"];
+  var vowelCount = 0
 
+for (let numberOfVowels = 0; numberOfVowels < arrayOfLetters.length; numberOfVowels++){
+  if (arrayOfVowels.includes(arrayOfLetters[numberOfVowels])){
+    vowelCount++
+  };
+}
 
-for (numberOfVowels = 0; numberOfVowels < arrayOfLetters.length; numberOfVowels++){
-  if (arrayOfVowels.includes(arrayOfLetters));
-};
-
-
-  return numberOfVowels
+  return vowelCount;
 }
 console.log(findVowelsCount("it may contain information that is privileged and confidential."))
